8af746fea49a1bb78e246520dd0a279b42e168e7
mike <mike.sifanele2.0@gmail.com>
2022-05-27 11:07:39 +0200

    Added constants

---
+++ MessageBroker/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageBroker
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Default uri, would split in production app.
+        /// </summary>
+        public static string GuestUriString = "amqp://guest:guest@localhost:5672";
+        public static string StarWarsQueueName = "starwars-queue";
+    }
+}
